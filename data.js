1767f6e2781288316003317b627ce37593fff9cb
lionel <lfillion@ingeod.fr>
2025-04-11 11:59:51 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,218 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250401_143032_00_034",
+      "name": "IMG_20250401_143032_00_034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0489167338525167,
+        "pitch": 0.3655337988307945,
+        "fov": 1.5386774479028458
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.130386887757405,
+          "pitch": 0.361628815941728,
+          "rotation": 9.42477796076938,
+          "target": "1-img_20250401_143142_00_035"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20250401_143142_00_035",
+      "name": "IMG_20250401_143142_00_035",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3594076815863225,
+          "pitch": 0.9513756265498579,
+          "rotation": 3.141592653589793,
+          "target": "2-img_20250401_144002_00_038"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20250401_144002_00_038",
+      "name": "IMG_20250401_144002_00_038",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.041423620318626675,
+          "pitch": 0.2267267635563588,
+          "rotation": 3.141592653589793,
+          "target": "3-img_20250401_143402_00_036"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9199679724261891,
+          "pitch": 0.6959136737780334,
+          "title": "RVS",
+          "text": "RVS DN 100&nbsp; fermé"
+        },
+        {
+          "yaw": -0.21827419259556535,
+          "pitch": 0.9534479665198443,
+          "title": "Compteur",
+          "text": "Compteur distribution"
+        },
+        {
+          "yaw": -0.3917283731447796,
+          "pitch": 0.3714008089111953,
+          "title": "Régulateur",
+          "text": "stabilisateur amont"
+        },
+        {
+          "yaw": 0.763327467427704,
+          "pitch": 0.8057118500329707,
+          "title": "RVS",
+          "text": "RVS DISTRIBUTION OUVERT"
+        },
+        {
+          "yaw": 0.5311479729227173,
+          "pitch": 0.6570242729858702,
+          "title": "RVS",
+          "text": "RVS DISTRIBUTION OUVERT"
+        }
+      ]
+    },
+    {
+      "id": "3-img_20250401_143402_00_036",
+      "name": "IMG_20250401_143402_00_036",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.52767290251224,
+          "pitch": 0.8861166999818089,
+          "title": "RVS",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.030840119492563,
+          "pitch": 1.113945078806946,
+          "title": "Boite à boues",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.924775661656124,
+          "pitch": 1.1324537258484852,
+          "title": "Compteur Adduction",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.578847787129553,
+          "pitch": 0.8961975575222993,
+          "title": "Savy remplissage",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.04362011305878788,
+          "pitch": 0.704225663563987,
+          "title": "RVS Ouvert",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Reservoir_Les_Emerys",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
